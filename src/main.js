8a3b84ce7b21e1c666a2713659b35d04128786ea
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-01 09:35:38 +0300

    end of part 4

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const vueApp = createApp(App);
+vueApp.use(router).mount("#app");
+
+//custom directive globally
+vueApp.directive("maxSize", (element, data) => {
+  element.style.fontSize = data.value + "px";
+});
+
+vueApp.directive("boldFont", (element, data) => {
+  element.style.fontWeight = data.value;
+});
