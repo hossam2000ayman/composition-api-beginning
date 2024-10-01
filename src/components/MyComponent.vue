0d7ff7433be8fb46a0cba47997c3b8c7bf4d4194
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-10-01 09:44:09 +0300

    end of part 2

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="component">
     <h1>This is component</h1>
-    <h1>Name is {{ props.userName }}</h1>
-    <h1>Age is {{ props.userAge }}</h1>
   </div>
 </template>
 
-<script setup>
-import { defineProps } from "vue";
-const props = defineProps({
-  userName: { type: String, required: true },
-  userAge: { type: Number, required: true },
-});
+<script>
+export default {
+  mounted() {
+    console.log("Router from component", this.$router);
+    console.log("Route from component", this.$route);
+  },
+};
 </script>
