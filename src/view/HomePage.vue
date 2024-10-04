c0a7a8c60beca47ab4a87077dcc334be73fef15e
Hossam Ayman <hhasanelbadry2000@gmail.com>
2024-10-04 23:55:44 +0300

    finalize part 6 which talk about watch in case of primitive and reference datatype

--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -2,18 +2,49 @@
   <div class="home">
     <h1>This is home page</h1>
     <h2>Counter is :: {{ counter }}</h2>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+
+    <button @click="counter++">Increase</button>
+    <button @click="counter--">Decrease</button>
+
+    <br />
+    <br />
+    <button @click="myArr.push(counter)">Push Counter value</button>
   </div>
 </template>
-
 <script setup>
-import counterFunction from "@/mixins/counter-mixins.js";
-const { counter, increment, decrement } = counterFunction();
+import { ref, watch } from "vue";
+
+const counter = ref(0);
+
+//Watch on Primitive datatype
+watch(
+  //Watch on which Data (fields)
+  () => {
+    return counter.value;
+  },
+  //Callback function to execute when the watched data changes
+  //it recieves 2 optional arguments the new value and the old value
+  (/* newValue, oldValue */) => {
+    // console.log(`oldValue :: ${oldValue}`);
+    // console.log(`newValue :: ${newValue}`);
+    console.log("counter is :: ", counter.value);
+  }
+);
+
+//Watch on reference datatype like array and object
+//so how we can watch on reference datatype like a array or object ?
+//by adding arguments of object which declare a deep is true instead of false
+const myArr = ref([]);
+watch(
+  () => myArr.value,
+  (newValue) => console.log(newValue),
+  { deep: true } //deep dive on reference datatype like array or object
+);
 </script>
 
 <style scoped>
 .home {
   text-align: center;
+  padding: 20px;
 }
 </style>
