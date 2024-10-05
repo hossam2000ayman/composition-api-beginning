7947c80b82ab7167e53132c768dd757259b2ae5f
Hossam Ayman <hhasanelbadry2000@gmail.com>
2024-10-05 17:00:01 +0300

    finish part 8 life cycle hooks

--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -1,39 +1,32 @@
 <template>
   <div class="home">
     <h1>This is home page</h1>
-    <hr />
-    <MyComponent />
+    <h2>Counter is :: {{ counter }}</h2>
+    <button @click="counter++">Increment</button>
+    <button @click="counter--">Decrement</button>
   </div>
 </template>
 <script setup>
-import MyComponent from "@/components/MyComponent.vue";
-import { provide, ref } from "vue";
+import { onBeforeMount, onBeforeUpdate, onMounted, onUpdated, ref } from "vue";
 
-const username = ref("Hossam AlBadry");
-const age = ref(24);
-const sayHello = () => {
-  console.log("Hello" + " " + username.value);
-};
+//Composition API is work faster than the Options API
+//because it access on the function itself and not access on the instance itself like on options API ("this")
 
-const friends = ref([
-  "moaz",
-  "amr",
-  "mahmoud",
-  "abdullah",
-  "mlegy",
-  "lina",
-  "amani",
-  "hossam",
-  "mostafa",
-]);
+const counter = ref(0);
 
-// provide(provider name , provider value) from parent component
-provide("userName", username.value);
-provide("userAge", age.value);
-provide("helloMethod", sayHello);
-provide("friendList", friends.value);
+onBeforeMount(() => {
+  console.log("Composition API Before Mount");
+});
+onMounted(() => {
+  console.log("Composition API Mounted");
+});
+onBeforeUpdate(() => {
+  console.log("Composition API Before Update");
+});
+onUpdated(() => {
+  console.log("Composition API Updated");
+});
 </script>
-
 <style scoped>
 .home {
   text-align: center;
